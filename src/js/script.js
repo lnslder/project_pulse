7be7e985fe3179081a05e66e3d6a222f0644d3b2
lnslder <golovatiy.pavel@gmail.com>
2023-02-07 12:54:07 +0200

    Final

--- src/js/script.js
+++ src/js/script.js
@@ -112,4 +112,12 @@ $(document).ready(function(){
         $('.pageup').fadeOut();
       }
     });
+
+/*     $("a(href=#up]").click(function() {
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    }); */
+
+    new WOW().init();
   });
